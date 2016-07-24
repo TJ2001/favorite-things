e5f9a8aa4d3b1ae399c1c13dba379edb4770c5e7
TJ2001 <jung.m.tim@gmail.com>
2016-07-23 17:44:57 -0700

    created a new array in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
-$(document).ready(function() {
-  $("#favthings").submit(function(event) {
+
+$("#favthings").submit(function(event) {
   // event.preventDefault();
     // var blanks = ["formI", "formI2", "formI3", "formI4"]
 
-    var x = new Array();
-    x[0] = document.getElementById("color").value;
-    x[1] = document.getElementById("food").value;
-    x[2] = document.getElementById("clothing").value;
-    x[3] = document.getElementById("drink").value;
+  var x = new Array();
+  x[0] = document.getElementById("color").value;
+  x[1] = document.getElementById("food").value;
+  x[2] = document.getElementById("clothing").value;
+  x[3] = document.getElementById("drink").value;
 
-    alert(x[0]);
-  });
+
+
+  // var cloneX = [x[1], x[0], x[2]]
+  var cloneX = [];
+  cloneX.push(x[1], x[0], x[2]);
+
+  $("ul").append("<h2>My Favorite Things:</h2>");
+  $("ul").append("<li>" + cloneX[0] + "</li>");
+  $("ul").append("<li>" + cloneX[1] + "</li>");
+  $("ul").append("<li>" + cloneX[2] + "</li>");
+  // alert(x[0]);
+    event.preventDefault();
 });
